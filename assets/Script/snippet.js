18cceb841dc1365a5f6260430ef7b87adb0ab366
Kevin Alexander Krefting <138679332+krefting-it@users.noreply.github.com>
2024-02-16 14:08:47 +0100

    update snippet.js

--- assets/Script/snippet.js
+++
@@ -1,8 +0,0 @@
-     document.getElementById('copyButton').addEventListener('click', function() {
-            var codeToCopy = '<a href=""><button>Download</button></a>';
-            navigator.clipboard.writeText(codeToCopy).then(function() {
-                alert('Der Code wurde in die Zwischenablage kopiert!');
-            }, function(err) {
-                console.error('Fehler beim Kopieren: ', err);
-            });
-        });
