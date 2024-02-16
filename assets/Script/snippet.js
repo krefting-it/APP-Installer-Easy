a6cae9a81e033f59e000dc841ac078a16e9af6c0
Kevin Alexander Krefting <138679332+krefting-it@users.noreply.github.com>
2024-02-16 13:55:23 +0100

    update snippet.js

---
+++ assets/Script/snippet.js
@@ -0,0 +1,18 @@
+function copyText() {
+    fetch('deine_datei.txt')
+    .then(response => response.text())
+    .then(text => {
+        navigator.clipboard.writeText(text)
+        .then(() => {
+            alert('Text wurde in die Zwischenablage kopiert!');
+        })
+        .catch(err => {
+            console.error('Fehler beim Kopieren des Textes: ', err);
+            alert('Fehler beim Kopieren des Textes!');
+        });
+    })
+    .catch(err => {
+        console.error('Fehler beim Lesen der Datei: ', err);
+        alert('Fehler beim Lesen der Datei!');
+    });
+}
