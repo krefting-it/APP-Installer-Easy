85d87823e4382d37441d901453e57702070e4fb8
Kevin Alexander Krefting <138679332+krefting-it@users.noreply.github.com>
2024-02-16 11:14:15 +0100

    update main.js

--- assets/Script/main.js
+++ assets/Script/main.js
@@ -1,1 +1,13 @@
+alert('Welcome by App installer Easy');
 
+function bundle1() {
+    alert('You Download now Bundle Version 1')
+}
+
+function bundle2() {
+    alert('You Download now Bundle Version 2')
+}
+
+function bundle3() {
+    alert('Bundle Version 3 is not available')
+}
